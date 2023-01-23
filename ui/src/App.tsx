5d9740f96c5223fc8ddb8dec5a7fee38176b4aef
Adam Gibson <adamsteven15+second_git@gmail.com>
2023-01-23 12:56:23 +0000

    Added page routes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,28 @@
+import { Box } from "@chakra-ui/react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-
+import { AddNewRecipe } from "./pages/AddNewRecipe/AddNewRecipe";
+import { Error } from "./pages/Error/Error";
 import { Home } from "./pages/Home/Home";
+import { ViewRecipe } from "./pages/ViewRecipe/ViewRecipe";
 
 const App = () => {
   return (
-    <div className="container">
+    <Box
+      w={["96%", "90%", null, "70%", "40%"]}
+      margin="auto"
+      display="flex"
+      flexDirection="column"
+      mt={5}
+    >
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/add-recipe" component={AddNewRecipe} />
+          <Route exact path="/view-recipe/:id" component={ViewRecipe} />
+          <Route path="*" component={Error} />
         </Switch>
       </Router>
-    </div>
+    </Box>
   );
 };
 
---
+++ ui/src/pages/AddNewRecipe/AddNewRecipe.tsx
@@ -0,0 +1,3 @@
+export const AddNewRecipe = () => {
+  return <div>AddNewRecipe</div>;
+};
