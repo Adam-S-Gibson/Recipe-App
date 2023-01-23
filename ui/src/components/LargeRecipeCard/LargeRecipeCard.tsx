123e63bb9c38c6519c8484f80ca3abcd205ac3b3
Adam Gibson <adamsteven15+second_git@gmail.com>
2023-01-23 14:15:40 +0000

    Updated styles

--- ui/src/components/LargeRecipeCard/LargeRecipeCard.tsx
+++ ui/src/components/LargeRecipeCard/LargeRecipeCard.tsx
@@ -1,25 +1,23 @@
-import {
-  Box,
-  Button,
-  Flex,
-  GridItem,
-  Image,
-  SimpleGrid,
-  Spacer,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Image, Button, SimpleGrid, GridItem } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { faStar as SolidStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useHistory } from "react-router-dom";
 import { Card } from "../../interfaces/Card";
 
 export const LargeRecipeCard = ({ title, rating, timeToCook, id }: Card) => {
   const pageHistory = useHistory();
-
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+    <Box
+      borderWidth="1px"
+      borderRadius="lg"
+      display="flex"
+      flexDirection="row"
+      maxW="max-content"
+      minW="min-content"
+    >
       <Image
+        maxW="40"
         src="https://media.istockphoto.com/id/1185879263/vector/recipe-book-hand-drawn-cover-vector-illustration.jpg?s=612x612&w=0&k=20&c=LMU-L5FcyKYdzPdB_ZNc0mQlFCwMyJM4iI94ZzBfpQM="
         alt="Tasty Recipe Image"
       />
@@ -32,41 +30,36 @@ export const LargeRecipeCard = ({ title, rating, timeToCook, id }: Card) => {
           lineHeight="tight"
           isTruncated
         >
-          <Flex>
-            <Text fontSize="2xl">{title}</Text>
-            <Spacer />
-            <Text fontSize="md" mt={1.5}>
-              {timeToCook} Mins
-            </Text>
-          </Flex>
-        </Box>
-
-        <Box display="flex" mt="1" alignItems="center">
           <SimpleGrid>
-            <GridItem>
-              {Array(5)
-                .fill("")
-                .map((_, i) => (
-                  <FontAwesomeIcon
-                    key={i}
-                    icon={i < rating ? SolidStar : faStar}
-                  />
-                ))}
-              <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                {Math.floor(Math.random() * 13)} reviews
-              </Box>
-            </GridItem>
-            <GridItem>
-              <Button
-                mt={3}
-                id="viewRecipeButton"
-                onClick={() => pageHistory.push(`/view-recipe/${id}`)}
-              >
-                View Recipe
-              </Button>
-            </GridItem>
+            <GridItem>{title}</GridItem>
+            <GridItem>{timeToCook} Mins</GridItem>
           </SimpleGrid>
         </Box>
+
+        <SimpleGrid columns={2}>
+          <GridItem colSpan={2}>
+            {Array(5)
+              .fill("")
+              .map((_, i) => (
+                <FontAwesomeIcon
+                  key={i}
+                  icon={i < rating ? SolidStar : faStar}
+                />
+              ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {Math.floor(Math.random() * 13)} reviews
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Button
+              size="sm"
+              id="viewRecipeButton"
+              onClick={() => pageHistory.push(`/view-recipe/${id}`)}
+            >
+              View Recipe
+            </Button>
+          </GridItem>
+        </SimpleGrid>
       </Box>
     </Box>
   );
