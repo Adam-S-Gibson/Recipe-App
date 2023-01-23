d55bf4fe135b06da08398e63ba7c2754483eea49
Adam Gibson <adamsteven15+second_git@gmail.com>
2023-01-23 14:26:39 +0000

    Updated to pull from api

--- ui/src/pages/Home/Home.tsx
+++ ui/src/pages/Home/Home.tsx
@@ -9,13 +9,30 @@ import {
   Text,
   useMediaQuery,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { LargeRecipeCard } from "../../components/LargeRecipeCard/LargeRecipeCard";
 import { SmallRecipeCard } from "../../components/SmallRecipeCard/SmallRecipeCard";
+import { Recipe } from "../../interfaces/Recipe";
 
 export const Home = () => {
   const pageHistory = useHistory();
   const [isLarge] = useMediaQuery("(max-width: 1440px)");
+
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const populatePage = async () => {
+    fetch("http://localhost:3080/api/recipes")
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data);
+        console.log(recipes);
+      });
+  };
+
+  useEffect(() => {
+    populatePage();
+  }, []);
 
   return (
     <Box>
@@ -27,31 +44,42 @@ export const Home = () => {
           Add New Recipe
         </Button>
       </SimpleGrid>
-      <Input my={2} placeholder="Search for recipe" />
-      <Divider mb={2} />
-      <SimpleGrid columns={[1, null, 2]} spacing="40px">
-        <Box>
-          {isLarge ? (
-            <SmallRecipeCard
-              title="test"
-              rating={Math.floor(Math.random() * 5) + 1}
-              timeToCook={"test"}
-              id={"test"}
-            />
-          ) : (
-            <LargeRecipeCard
-              title="test"
-              rating={Math.floor(Math.random() * 5) + 1}
-              timeToCook={"test"}
-              id={"test"}
-            />
-          )}
-        </Box>
-      </SimpleGrid>
-      <Text textAlign="center">
-        {" "}
-        Oh no! you've got no recipes, please add some with the button above! 🙌
-      </Text>
+      <Input
+        mt={2}
+        id="searchBar"
+        placeholder="Search By Ingredient or Name"
+        onChange={() => console.log("Searched Something")}
+      />
+      <Divider my={2} />
+      {recipes.length > 0 ? (
+        <SimpleGrid columns={[1, null, 2]} spacing="40px">
+          {recipes.map((item, i) => (
+            <Box key={i}>
+              {isLarge ? (
+                <SmallRecipeCard
+                  title={item.name}
+                  rating={Math.floor(Math.random() * 5) + 1}
+                  timeToCook={parseInt(item.time_to_make)}
+                  id={item.id}
+                />
+              ) : (
+                <LargeRecipeCard
+                  title={item.name}
+                  rating={Math.floor(Math.random() * 5) + 1}
+                  timeToCook={parseInt(item.time_to_make)}
+                  id={item.id}
+                />
+              )}
+            </Box>
+          ))}
+        </SimpleGrid>
+      ) : (
+        <Text textAlign="center">
+          {" "}
+          Oh no! you've got no recipes, please add some with the button above!
+          🙌{" "}
+        </Text>
+      )}
     </Box>
   );
 };
