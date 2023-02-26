ad6082db26ea4431cb68ab20e4b448421047f60c
Adam Gibson <adamsteven15+second_git@gmail.com>
2023-02-26 22:28:12 +0000

    Updated git ignore and readme

--- ui/src/components/Footer/Footer.tsx
+++ ui/src/components/Footer/Footer.tsx
@@ -3,8 +3,7 @@ import { Text } from "@chakra-ui/react";
 export const Footer = () => {
   return (
     <Text textAlign="center">
-      Copyright &copy; {new Date().getFullYear()} Sero Kitchen All Rights
-      Reserved
+      Copyright &copy; {new Date().getFullYear()} All Rights Reserved
     </Text>
   );
 };
